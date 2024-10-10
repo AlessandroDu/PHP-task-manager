972c5c6208ad7e11eb78e4a4fcf4733f0fc166a7
Alessandro <duccialessandro001@gmail.com>
2024-10-10 12:11:24 +0400

    CONTROLLERS: Finished project controller logic. Ready for VIEWS.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -17,31 +17,47 @@ class ProjectController extends Controller
 
     public function create()
     {
-
+        return view('projects.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
 
+        Project::create($validated);
+
+        return redirect()->route('projects.index');
     }
 
-    public function edit()
+    public function show(Project $project)
     {
-
+        return view('projects.show', compact($project));
     }
 
-    public function update()
+    public function edit(Project $project)
     {
-
+        return view('projects.edit', compact($project));
     }
 
-    public function show()
+    public function update(Request $request, Project $project)
     {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
 
+        $project->update($validated);
+
+        return redirect()->route('projects.index');
     }
 
-    public function destroy()
+    public function destroy(Project $project)
     {
+        $project->delete();
 
+        return redirect()->route('projects.index');
     }
 }
